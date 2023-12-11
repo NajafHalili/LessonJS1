50fbd484387e9f8fbe50895c66a7a3019ade1e02
unknown <necef.xelilov@bk.ru>
2023-12-11 21:05:00 +0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+////console.log("Hello world")
+let a = 22
+let b = 14
+ a= a+b  
+ b= a-b 
+ a= a-b 
+ console.log(a,b)
+
+
+
+let d = 2005
+let g = 2025
+let age1 = g-d
+let age2= g-d-1
+console.log( age1)
+console.log( age2)
